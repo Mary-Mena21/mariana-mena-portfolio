3700387f45ee5381f22ffe4a8aca470157a78351
Mariana Mena <maranashokry@ahoo.com>
2024-12-03 14:37:04 -0600

    install framer-motion

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
 // import Image from "next/image";
 
+import Hero from '@/components/Hero';
+
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <div>
-          <h1>Hello</h1>
+          <Hero />
         </div>
       </div>
     </main>
   );
 }
+//ctr+k =>next.js
+//npm install -g npm@10.9.1
+//npm i framer-motion clsx tailwind-merge --force
+//npm config set legacy-peer-deps true     ** works**
