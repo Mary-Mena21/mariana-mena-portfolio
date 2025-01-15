b71f0dcdf019ae162288bcfc608e912d5ae4d2bd
Mariana Mena <maranashokry@ahoo.com>
2025-01-15 15:14:21 -0600

    add button and floating navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Spotlight } from './ui/Spotlight';
 import { TextGenerateEffect } from './ui/TextGenerateEffect';
+import {Button, buttonVariants} from './ui/button';
 
 function Hero() {
   return (
@@ -18,7 +19,7 @@ function Hero() {
         <Spotlight className="-top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0" >
+      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-0 left-0">
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
       </div>
@@ -27,12 +28,24 @@ function Hero() {
           <h2 className="uppercase tracking-widest text-center text-xs text-blue-100 max-w-80">
             Dynamic Web Magic Next.js
           </h2>
-          <TextGenerateEffect className='text-center text-[40px] md:text-5xl lg:text-6xl'
-            words="Transforming Concepts into Seamless Digital Experiences" />  
+          <TextGenerateEffect
+            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            words="Transforming Concepts into Seamless Digital Experiences"
+          />
           <p className="text-center text-sm md:tracking-wider mb-4 md:text-lg lg:text-2xl">
             Hi I&apos;m Mariana, a passionate web developer and designer.
             Let&apos;s collaborate to create unique, engaging experiences.
           </p>
+          <a href="https://github.com/marymena21s-projects/mariana-mena-portfolio">
+            <Button/>
+
+            {/* <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+  <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+  <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+    Border Magic
+  </span>
+</button> */}
+          </a>
         </div>
       </div>
     </div>
